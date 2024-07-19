079053a59038a45a270ac3dcb260bee501b41e99
ivantelizhenko <telizenkoo@gmail.com>
2024-07-19 16:31:34 +0300

    add ingredients is restricted and more friend;y

--- src/features/cart/AddIngredient.jsx
+++ src/features/cart/AddIngredient.jsx
@@ -1,20 +1,44 @@
 import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { addIngredient } from './cartSlice';
+import { useFetcher } from 'react-router-dom';
 
 function AddIngredient({ pizzaId }) {
   const dispatch = useDispatch();
   const [ingredient, setIngredient] = useState('');
 
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
+
+  const allIngredients = [];
+  fetcher?.data?.forEach(pizza =>
+    pizza.ingredients.forEach(ingredient => allIngredients.push(ingredient)),
+  );
+  const allIngredientsSet = [...new Set(allIngredients)];
+
   return (
     <div>
-      <input
+      <select
+        disabled={fetcher.state === 'loading'}
         onChange={e => setIngredient(e.target.value)}
         value={ingredient}
         placeholder="ingredient"
         className="rounded-full border border-stone-200 px-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400"
-      />
+      >
+        <option value="">Please choose an ingredient</option>{' '}
+        {allIngredientsSet.map(el => (
+          <option key={el} value={el}>
+            {el}
+          </option>
+        ))}
+      </select>
       <Button
         type="secondarySmallYellow"
         onClick={() => {
